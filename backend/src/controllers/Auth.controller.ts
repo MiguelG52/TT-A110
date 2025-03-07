d2551ccb52dd94b390c7df2fcc8a640e93fa5d26
Miguel Gonzalez Sydech <mikigonza52@gmail.com>
2025-03-07 14:20:33 -0600

    feat:confirm user account

--- backend/src/controllers/Auth.controller.ts
+++ backend/src/controllers/Auth.controller.ts
@@ -6,13 +6,13 @@ import { EmailService } from '../services/Email.Service';
 
 export class AuthController {
     static async createUser(req: Request, res: Response) {    
+        //validate if email is already registered
         const { email, password } = req.body;
         const userExists = await User.findOne({ where: { email } });
         if (userExists) {
             const error = new Error('El usuario ya existe');
             res.status(400).json({error:error.message});
         }
-
         // Create user
         try {
             const user = new User(req.body);
@@ -20,7 +20,7 @@ export class AuthController {
             const token = generateToken();
             user.dataValues.token = token;
             user.dataValues.password = passwordEncrypted;
-            //await user.save();
+            await user.save();
             await EmailService.sendEmailConfirmacion({
                 name:user.dataValues.name,
                 email: user.dataValues.email,
@@ -29,12 +29,27 @@ export class AuthController {
             res.status(201).json('Cuenta creada exitosamente');
         } catch (error) {
             res.status(500).json({
-                message:"Error en el servidor",
+                message:"Error al crear usuario",
             });
         }
     }
 
-    static async confirmAccount(req: Request, res:Response){
-    
+    static async confirmAccount(req: Request, res: Response) {
+        try {
+            const { token } = req.body;
+            const user = await User.findOne({ where: { token } });
+            if (!user) {
+                res.status(404).json({ error: "Token no válido" });
+            }
+            // update user fields
+            await user.update({
+                token:null,
+                isVerified:true
+            })
+            res.json({ message: "Cuenta verificada correctamente" });
+        } catch (error) {
+            res.status(500).json({ error: "Error al confirmar la cuenta" });
+        }
     }
+    
 }
