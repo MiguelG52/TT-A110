fd59925a22825c34b67173554b7b2cad27f2e01f
Miguel Gonzalez Sydech <mikigonza52@gmail.com>
2025-03-18 13:24:39 -0600

    fix: bearer token

--- backend/src/helpers/jwt.helper.ts
+++ backend/src/helpers/jwt.helper.ts
@@ -1,8 +1,13 @@
 import jwt from "jsonwebtoken";
 
-export const generateJWTToken = (id: string) =>{
+export const generateJWT = async (id: string) =>{
     const token = jwt.sign({id}, process.env.JWT_SECRET,{
         expiresIn:'10d'
     })
     return token
+}
+
+export const verfyJWT = async(encodedToken:string)=>{
+    const user = jwt.verify(encodedToken, process.env.JWT_SECRET)
+    return user
 }
