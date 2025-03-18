9f4db03762b3f396b72bb8a441104e77ad6f1a1a
Miguel Gonzalez Sydech <mikigonza52@gmail.com>
2025-03-18 16:58:44 -0600

    fix: commits editor >:v

--- frontend/models/schemas.ts
+++ frontend/models/schemas.ts
@@ -1,7 +1,12 @@
 import {z} from 'zod'
 
-export const authFormSchema = z.object({
+export const authFormSchema = (type:string) => z.object({
+    name: type==="sign-in" ? z.string().optional(): z.string().min(3,{message:'El nombre es requerido'}),
+    lastName:type==="sign-in" ? z.string().optional(): z.string().min(3,{message:'Los apellidos son requeridos'}),
+    username:type==="sign-in" ? z.string().optional(): z.string().min(3,{message:'El nombre de usuario es requerido'}),
+    role:type==="sign-in" ? z.string().optional(): z.string().min(1,{message: 'El rol es requerido'}),
+
     email: z.string().email({ message: 'El correo electrónico no es válido' }),
-    password: z.string()
-    .min(8, { message: 'La contraseña debe tener al menos 8 caracteres' })
+    password: z.string().min(8, { message: 'La contraseña debe tener al menos 8 caracteres' })
 })
+
