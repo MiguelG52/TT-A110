8fbc6a051c90570e48677f0b696cd676398914ed
Rodrigo Salazar <rodrigosalazarvega4@gmail.com>
2025-03-27 15:54:29 -0600

    fix home page header full width

--- frontend/app/home/layout.tsx
+++ frontend/app/home/layout.tsx
@@ -27,21 +27,20 @@ export default function UserProfileLayout({
 }) {
     return (
         <SidebarProvider>
-            <div className="flex min-h-screen">
+            <div className="flex min-h-screen w-full">
                 {/* Sidebar a la izquierda */}
                 <AppSidebar />
 
                 {/* Contenedor del contenido */}
-                <div className="flex flex-col flex-1 min-h-screen">
+                <div className="flex flex-col flex-1 min-h-screen w-full">
                     {/* Header con botón de menú */}
-                    <header className="bg-blue-600 text-white p-4 shadow-md flex items-center">
+                    <header className="bg-blue-600 text-white p-4 shadow-md flex items-center w-full">
                         <SidebarTrigger className="mr-4 text-white" />
                         <h1 className="text-xl font-bold">Odemia</h1>
                     </header>
 
-                    {/* Contenido principal que ocupa toda la pantalla */}
-                    {/* <main className="flex-1 bg-gray-100 p-6"> */}
-                    <main>
+                    {/* Contenido principal con ancho completo */}
+                    <main className="flex-1 flex flex-col items-center justify-center bg-gray-100 p-6 w-full">
                         {children}
                     </main>
                 </div>
--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -2,8 +2,10 @@ import { redirect } from "next/dist/server/api-utils";
 
 export default function DashboardPage() {
     return (
-        <div className="h-full w-full flex flex-col items-center justify-center bg-white p-6">
+        <div className="flex-1 w-full h-full flex flex-col items-center justify-center bg-white p-6">
             <h1 className="text-2xl font-bold">Bienvenido</h1>
         </div>
     );
 }
+
+
