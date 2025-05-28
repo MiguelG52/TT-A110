030710ec7741a278af645b90a427fbf94a733100
Your Name <you@example.com>
2025-05-27 19:37:54 -0600

    feat: unit test 100%

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -6,6 +6,20 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  coverageReporters: [
+    'text',
+    ['text-summary', {
+      skipFull: true,
+      fileCoverageThreshold: 80
+    }]
+  ],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.{ts,js}',
+    '!src/**/*.d.ts',
+    '!src/servers/**', // Excluir archivos de servidor si es necesario
+  ],
   testMatch: ['**/**/*.test.ts'],
   verbose: true,
   forceExit: true,
